092d85b4dca1ad77824d346ca1777dc5f5a96358
huangrunhong <huang.run.hong.1986@gmail.com>
2023-12-07 15:29:24 +0100

    details Page version 1

--- src/components/Pokemons/PokemonGallery.jsx
+++ src/components/Pokemons/PokemonGallery.jsx
@@ -2,29 +2,27 @@ import { useContext } from "react";
 import { FetchData, LoadingDone } from "../../context/Context";
 import { v4 as uuidv4 } from "uuid";
 import PokemonCard from "../pokemonCard/PokemonCard";
-import "./Gallery.scss"
+import "./Gallery.scss";
 
 const PokemonGallery = () => {
-	const myData = useContext(FetchData);
-	const loading = useContext(LoadingDone);
-	console.log(loading);
-	return (
-		<section className='gallery'>
-			{myData
-				? myData.data.map((pokemon) => (
-						<PokemonCard
-							key={uuidv4()}
-							name={pokemon.species.name}
-							image={
-								pokemon.sprites.other["official-artwork"]
-									.front_shiny
-							}
-							weight={pokemon.weight}
-						/>
-				  ))
-				: console.log("loading")}
-		</section>
-	);
+  const myData = useContext(FetchData);
+  const loading = useContext(LoadingDone);
+  // console.log(myData);
+  return (
+    <section className="gallery">
+      {myData
+        ? myData.data.map((pokemon) => (
+            <PokemonCard
+              key={uuidv4()}
+              name={pokemon.species.name}
+              image={pokemon.sprites.other["official-artwork"].front_shiny}
+              weight={pokemon.weight}
+              id={pokemon.id}
+            />
+          ))
+        : console.log("loading")}
+    </section>
+  );
 };
 
 export default PokemonGallery;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,61 +6,57 @@ import { names } from "../data/Names";
 import { FetchData, LoadingDone } from "../../context/Context";
 
 const Header = () => {
-	const [searchInput, setSearchInput] = useState("");
-	const fetchState = useContext(FetchData);
-	const [foundId, setFoundId] = useState([]);
-    const loadingState = useContext(LoadingDone)
+  const [searchInput, setSearchInput] = useState("");
+  const fetchState = useContext(FetchData);
+  const [foundId, setFoundId] = useState([]);
+  const loadingState = useContext(LoadingDone);
 
-	useEffect(() => {
-		const urls = [];
-		names.forEach((name) => urls.push(name.url));
-		setFoundId(urls);
-	}, []);
+  useEffect(() => {
+    const urls = [];
+    names.forEach((name) => urls.push(name.url));
+    setFoundId(urls);
+  }, []);
 
+  useEffect(() => {
+    const pokemonArray = [];
+    foundId.forEach((url) => {
+      fetch(`${url}`)
+        .then((response) => response.json())
+        .then((data) => {
+          pokemonArray.push(data);
+          fetchState.setData(pokemonArray);
+          console.log("fetch");
+          if (pokemonArray.length === 1192) {
+            loadingState.setLoading(true);
+          }
+        })
+        .catch((err) => console.log(err));
+    });
+  }, [foundId]);
 
-	useEffect(() => {
-        const pokemonArray = []
-		foundId.forEach((url) => {
-			fetch(`${url}`)
-				.then((response) => response.json())
-				.then((data) => {
-                    pokemonArray.push(data)
-                    fetchState.setData(pokemonArray);
-                    console.log("fetch")
-                    if (pokemonArray.length === 1192) {
-                        loadingState.setLoading(true)
-                    }
-                })
-				.catch((err) => console.log(err));
-		});
-	}, [foundId]);
+  console.log(fetchState);
 
-	console.log(fetchState);
-
-	return (
-		<header>
-			<nav>
-				<Link>
-					<img
-						src={logo}
-						alt=''
-					/>
-				</Link>
-				<div>
-					<NavLink>Home</NavLink>
-					<NavLink>Types</NavLink>
-					<NavLink>Our Team</NavLink>
-				</div>
-			</nav>
-			<section>
-				<input
-					type='text'
-					placeholder='Search...'
-					onChange={(e) => setSearchInput(e.target.value)}
-				/>
-			</section>
-		</header>
-	);
+  return (
+    <header>
+      <nav>
+        <Link>
+          <img src={logo} alt="" />
+        </Link>
+        <div>
+          <NavLink>Home</NavLink>
+          <NavLink>Types</NavLink>
+          <NavLink>Our Team</NavLink>
+        </div>
+      </nav>
+      <section>
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+      </section>
+    </header>
+  );
 };
 
 export default Header;
