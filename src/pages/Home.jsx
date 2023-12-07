9e16ed0a8f73beee89e5f34b63bac2555ada28d0
MoniqueHeusinger <m.heusinger1821@gmail.com>
2023-12-07 14:57:00 +0100

    Styling Version 1 fertig

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import PokemonGallery from "../components/Pokemons/PokemonGallery";
+import Filterbar from "../components/filterbar/Filterbar";
 import "./Home.scss"
 const Home = () => {
 	return (
 		<main>
-			<PokemonGallery />
+			<fieldset>
+				<legend>
+					{/* Pokemon Logo */}
+					<img src="./../public/img/pokemon_logo.gif" alt="Pokemon Logo" />
+					
+					</legend>
+				{/* <Filterbar />	 */}
+				<PokemonGallery />
+			</fieldset>
+			
 		</main>
 	);
 };
