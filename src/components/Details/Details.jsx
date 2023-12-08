c92d76a4d17b356b48e3b3353f8a3913580cffd2
huangrunhong <huang.run.hong.1986@gmail.com>
2023-12-08 13:08:59 +0100

    type color added

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -4,39 +4,153 @@ import { FetchData } from "../../context/Context";
 import "./Details.css";
 import { Link } from "react-router-dom";
 
+const styles = {
+  bug: {
+    boxShadow: "inset 0px 39px 0px -24px #a4e271",
+    backgroundColor: "#74b807",
+    border: "1px solid #74b807",
+    textShadow: "0px 1px 0px #528009",
+  },
+  dark: {
+    boxShadow: "inset 0px 39px 0px -24px #4e4e4e",
+    backgroundColor: "#303030",
+    border: "1px solid #303030",
+    textShadow: "0px 1px 0px #7d7d7d",
+  },
+  dragon: {
+    boxShadow: "inset 0px 39px 0px -24px #0876e4",
+    backgroundColor: "#00458A",
+    border: "1px solid #00458A",
+    textShadow: "0px 1px 0px #2d7ccb",
+  },
+  electric: {
+    boxShadow: "inset 0px 39px 0px -24px #ffe713",
+    backgroundColor: "#e4cd01",
+    border: "1px solid #e4cd01",
+    textShadow: "0px 1px 0px #e0d35d",
+  },
+  fairy: {
+    boxShadow: "inset 0px 39px 0px -24px #FFC2F9",
+    backgroundColor: "#f98aee",
+    border: "1px solid #f98aee",
+    textShadow: "0px 1px 0px #f7c8f3",
+  },
+  fighting: {
+    boxShadow: "inset 0px 39px 0px -24px #ed4242",
+    backgroundColor: "#E40000",
+    border: "1px solid #E40000",
+    textShadow: "0px 1px 0px #f14d4d",
+  },
+  fire: {
+    boxShadow: "inset 0px 39px 0px -24px #fcbb59",
+    backgroundColor: "#FF9900",
+    border: "1px solid #FF9900",
+    textShadow: "0px 1px 0px #fcbb59",
+  },
+  flying: {
+    boxShadow: "inset 0px 39px 0px -24px #e5edee",
+    backgroundColor: "#CCDADD",
+    border: "1px solid #CCDADD",
+
+    textShadow: "0px 1px 0px #fff",
+  },
+  ghost: {
+    boxShadow: "inset 0px 39px 0px -24px #8e2d9e",
+    backgroundColor: "#5A1E64",
+    border: "1px solid #5A1E64",
+    textShadow: "0px 1px 0px #a94eb8",
+  },
+  grass: {
+    boxShadow: "inset 0px 39px 0px -24px #78bf31",
+    backgroundColor: "#57921C",
+    border: "1px solid #57921C",
+    textShadow: "0px 1px 0px #8ed448",
+  },
+  ground: {
+    boxShadow: "inset 0px 39px 0px -24px #d18411",
+    backgroundColor: "#965A00",
+    border: "1px solid #965A00",
+    textShadow: "0px 1px 0px #e69c2f",
+  },
+  ice: {
+    boxShadow: "inset 0px 39px 0px -24px #aee2f9",
+    backgroundColor: "#6AD2FF",
+    border: "1px solid #6AD2FF",
+    textShadow: "0px 1px 0px #aee2f9",
+  },
+  normal: {
+    boxShadow: "inset 0px 39px 0px -24px #d5d5d5",
+    backgroundColor: "#B3B3B3",
+    border: "1px solid #B3B3B3",
+    textShadow: "0px 1px 0px #eeeeee",
+  },
+  plant: {
+    boxShadow: "inset 0px 39px 0px -24px #8bf124",
+    backgroundColor: "#70DF00",
+    border: "1px solid #70DF00",
+    textShadow: "0px 1px 0px #9bee46",
+  },
+  poison: {
+    boxShadow: "inset 0px 39px 0px -24px #df17e3",
+    backgroundColor: "#AB00AE",
+    border: "1px solid #AB00AE",
+    textShadow: "0px 1px 0px #f45cf7",
+  },
+  psychic: {
+    boxShadow: "inset 0px 39px 0px -24px #faaef2",
+    backgroundColor: "#FF81F2",
+    border: "1px solid #FF81F2",
+    textShadow: "0px 1px 0px #faaef2",
+  },
+  rock: {
+    boxShadow: "inset 0px 39px 0px -24px #f7d06a",
+    backgroundColor: "#E1B237",
+    border: "1px solid #E1B237",
+    textShadow: "0px 1px 0px #f7d06a",
+  },
+  steel: {
+    boxShadow: "inset 0px 39px 0px -24px #4e8490",
+    backgroundColor: "#2A4950",
+    border: "1px solid #2A4950",
+    textShadow: "0px 1px 0px #649aa6",
+  },
+  water: {
+    boxShadow: "inset 0px 39px 0px -24px #1cb0ee",
+    backgroundColor: "#00A0E4",
+    border: "1px solid #00A0E4",
+    textShadow: "0px 1px 0px #6acffa",
+  },
+};
+
 const Details = () => {
-	const pokeParams = useParams();
-	const [poke, setPoke] = useState({});
+  const pokeParams = useParams();
+  const [poke, setPoke] = useState({});
 
-	const myData = useContext(FetchData);
+  const myData = useContext(FetchData);
 
-	const singelData = myData.data.find(
-		(dataItem) => dataItem.id.toString() === pokeParams.id.toString(),
-	);
+  const singelData = myData.data.find(
+    (dataItem) => dataItem.id.toString() === pokeParams.id.toString()
+  );
 
-	useEffect(() => {
-		fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeParams.id}`)
-			.then((res) => res.json())
-			.then((data) => setPoke(data));
-	}, [pokeParams]);
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeParams.id}`)
+      .then((res) => res.json())
+      .then((data) => setPoke(data));
+  }, [pokeParams]);
 
+  return (
+    <>
+      {singelData ? (
+        <section className="Details">
+          <div>
+            <h3>{singelData.species.name} </h3>
+            <h3>#00{singelData.id} </h3>
+          </div>
+          <img
+            src={singelData.sprites.other["official-artwork"].front_shiny}
+            alt=""
+          />
 
-	return (
-		<>
-			{singelData ? (
-				<section className='Details'>
-					<div>
-						<h3>{singelData.species.name} </h3>
-						<h3>#00{singelData.id} </h3>
-					</div>
-					<img
-						src={
-							singelData.sprites.other["official-artwork"]
-								.front_shiny
-						}
-						alt=''
-					/>
-              
           <div>
             <h4> Height: </h4>
             <p>{singelData.height} m</p>
@@ -56,8 +170,12 @@ const Details = () => {
           <div>
             <h4>Type:</h4>
             <div>
-              <p>{singelData.types[0]?.type.name}</p>
-              <p>{singelData.types[1]?.type.name}</p>
+              <p style={styles[singelData?.types[0]?.type.name]}>
+                {singelData.types[0]?.type.name}
+              </p>
+              <p style={styles[singelData?.types[1]?.type.name]}>
+                {singelData.types[1]?.type.name}
+              </p>
               {singelData.types.length > 2 ? (
                 <Link to={`/more-types/${pokeParams.id}`}>
                   <p>more types</p>
--- src/components/filterOptions/FilterOptions.jsx
+++ src/components/filterOptions/FilterOptions.jsx
@@ -1,30 +1,222 @@
 import "./FilterOptions.css";
 import { Link } from "react-router-dom";
 const FilterOptions = () => {
+  var types = [
+    {
+      type: "bug",
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #a4e271",
+        backgroundColor: "#74b807",
+        border: "1px solid #74b807",
+        textShadow: "0px 1px 0px #528009",
+      },
+    },
+    {
+      type: "dark",
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #4e4e4e",
+        backgroundColor: "#303030",
+        border: "1px solid #303030",
+        textShadow: "0px 1px 0px #7d7d7d",
+      },
+    },
+    {
+      type: "dragon",
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #0876e4",
+        backgroundColor: "#00458A",
+        border: "1px solid #00458A",
+        textShadow: "0px 1px 0px #2d7ccb",
+      },
+    },
+    {
+      type: "electric",
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #ffe713",
+
+        backgroundColor: "#e4cd01",
+        border: "1px solid #e4cd01",
+
+        textShadow: "0px 1px 0px #e0d35d",
+      },
+    },
+    {
+      type: "fairy",
+
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #FFC2F9",
+
+        backgroundColor: "#f98aee",
+        border: "1px solid #f98aee",
+
+        textShadow: "0px 1px 0px #f7c8f3",
+      },
+    },
+    {
+      type: "fighting",
+
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #ed4242",
+
+        backgroundColor: "#E40000",
+        border: "1px solid #E40000",
+        textShadow: "0px 1px 0px #f14d4d",
+      },
+    },
+    {
+      type: "fire",
+
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #fcbb59",
+
+        backgroundColor: "#FF9900",
+        border: "1px solid #FF9900",
+        textShadow: "0px 1px 0px #fcbb59",
+      },
+    },
+    {
+      type: "flying",
+
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #e5edee",
+
+        backgroundColor: "#CCDADD",
+        border: "1px solid #CCDADD",
+      },
+    },
+    {
+      type: "ghost",
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #8e2d9e",
+        backgroundColor: "#5A1E64",
+        border: "1px solid #5A1E64",
+
+        textShadow: "0px 1px 0px #a94eb8",
+      },
+    },
+    {
+      type: "grass",
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #78bf31",
+        backgroundColor: "#57921C",
+        border: "1px solid #57921C",
+        textShadow: "0px 1px 0px #8ed448",
+      },
+    },
+    {
+      type: "ground",
+
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #d18411",
+        backgroundColor: "#965A00",
+        border: "1px solid #965A00",
+        textShadow: "0px 1px 0px #e69c2f",
+      },
+    },
+    {
+      type: "ice",
+
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #aee2f9",
+        backgroundColor: "#6AD2FF",
+        border: "1px solid #6AD2FF",
+        textShadow: "0px 1px 0px #aee2f9",
+      },
+    },
+    {
+      type: "normal",
+
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #d5d5d5",
+        backgroundColor: "#B3B3B3",
+        border: "1px solid #B3B3B3",
+        textShadow: "0px 1px 0px #eeeeee",
+      },
+    },
+    {
+      type: "plant",
+      color: "#70DF00",
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #8bf124",
+
+        backgroundColor: "#70DF00",
+        border: "1px solid #70DF00",
+
+        textShadow: "0px 1px 0px #9bee46",
+      },
+    },
+    {
+      type: "poison",
+      color: "#AB00AE",
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #df17e3",
+
+        backgroundColor: "#AB00AE",
+        border: "1px solid #AB00AE",
+
+        textShadow: "0px 1px 0px #f45cf7",
+      },
+    },
+    {
+      type: "psychic",
+
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #faaef2",
+
+        backgroundColor: "#FF81F2",
+        border: "1px solid #FF81F2",
+
+        textShadow: "0px 1px 0px #faaef2",
+      },
+    },
+    {
+      type: "rock",
+
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #f7d06a",
+
+        backgroundColor: "#E1B237",
+        border: "1px solid #E1B237",
+
+        textShadow: "0px 1px 0px #f7d06a",
+      },
+    },
+    {
+      type: "steel",
+
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #4e8490",
+
+        backgroundColor: "#2A4950",
+        border: "1px solid #2A4950",
+        textShadow: "0px 1px 0px #649aa6",
+      },
+    },
+    {
+      type: "water",
+      buttonStyle: {
+        boxShadow: "inset 0px 39px 0px -24px #1cb0ee",
+
+        backgroundColor: "#00A0E4",
+        border: "1px solid #00A0E4",
+
+        textShadow: "0px 1px 0px #6acffa",
+      },
+    },
+  ];
   return (
     <section className="filterOptions">
-      <h1>Type</h1>
-      <div>
-        <Link to="/filter-results/bug">BUG</Link>
-        <Link to="/filter-results/dark">DARK</Link>
-        <Link to="/filter-results/dragon">DRAGON</Link>
-        <Link to="/filter-results/electric">ELECTRIC</Link>
-        <Link to="/filter-results/fairy">FAIRY</Link>
-        <Link to="/filter-results/fighting">FIGHTING</Link>
-        <Link to="/filter-results/fire">FIRE</Link>
-        <Link to="/filter-results/flying">FLYING</Link>
-        <Link to="/filter-results/ghost">GHOST</Link>
-        <Link to="/filter-results/grass">GRASS</Link>
-        <Link to="/filter-results/ground">GROUND</Link>
-        <Link to="/filter-results/ice">ICE</Link>
-        <Link to="/filter-results/normal">NORMAL</Link>
-        <Link to="/filter-results/plant">PLANT</Link>
-        <Link to="/filter-results/poison">POISON</Link>
-        <Link to="/filter-results/psychic">PSYCHIC</Link>
-        <Link to="/filter-results/rock">ROCK</Link>
-        <Link to="/filter-results/steel">STEEL</Link>
-        <Link to="/filter-results/water">WATER</Link>
-      </div>
+      {types ? (
+        <div>
+          {types.map((item, index) => (
+            <Link key={index} to={`/filter-results/${item.type}`}>
+              <p style={item.buttonStyle}>{item.type} </p>
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <p></p>
+      )}
     </section>
   );
 };
